b0fbd0951f8d50d45caf02567f74a572e4e44056
Padmesh dwivedi <dwivedipadmesh9@gmail.com>
2025-08-02 20:25:50 +0530

    Update before version bump

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 const freecurrencyapi = new Freecurrencyapi('fca_live_RCXO0xNxoInEVYv6JLub3wbyhNDzaoqkWRHMmgiY');
 
-convertCurrency("INR","USD",3);
+let var1=convertCurrency("INR","USD",3);
+
+let var2=convertCurrency("USD","INR",3);
+console.log(var1,var2);
 async function convertCurrency(fromCurrency,toCurrecny,units) {
     const res= await freecurrencyapi.latest({
                 base_currency: fromCurrency,
